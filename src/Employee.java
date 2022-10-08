f94b0b55e61926a116951735db316542ef48ccf2
Daria <m-alberts@yandex.ru>
2022-10-08 16:51:33 +0300

    Курсовая

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+public class Employee {
+    private final String fullName;
+    private int department;
+    private int salary;
+    private static int counter = 0;
+    private final int id;
+
+    public Employee (String fullName, int department, float salary){
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = (int) salary;
+        this.id = ++counter;
+    }
+    public int getId(){
+        return this.id;
+    }
+
+    public String getFullName(){
+        return this.fullName;
+    }
+
+    public int getDepartment(){
+        return this.department;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public void setDepartment(int department){
+        this.department = department;
+    }
+
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудники{" +
+                "ФИО: " + fullName + '\'' +
+                ", Отдел: " + department +
+                ", Зарплата: " + salary +
+                ", ID: " + id +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
+import java.util.Arrays;
+
 public class Main {
+
+    private static final Employee[] employees = new Employee[10];
+
     public static void main(String[] args) {
+        employees[0] = new Employee("Середа Виктор Данилович", 1, 3500);
+        employees[1] = new Employee("Козелков Павел Кондратьевич", 2, 4300);
+        employees[2] = new Employee("Козелкова Ирина Кондратьевна", 2, 5400);
+        employees[3] = new Employee("Панферова Екатерина Сергеевна", 3, 7000);
+        employees[4] = new Employee("Сорокина Юлия Алексеевна", 3, 7600);
+        employees[5] = new Employee("Кутейщикова Варвара Николаевна", 4, 6600);
+        employees[6] = new Employee("Копаныгин Михаил Васильевич", 4, 6900);
+        employees[7] = new Employee("Курочкин Александр Иванович", 5, 3200);
+        employees[8] = new Employee("Филатов Сергей Сергеевич", 5, 3500);
+        employees[9] = new Employee("Окорокова Людмила Владимировна", 5, 2300);
+        allEmployees();
+        System.out.println("Сумма всех зарплат: " + sumSalary());
+        System.out.println("Сотрудник с максимальной зарплатой: " + withMaxSalary());
+        System.out.println("Сотрудник с минимальной зарплатой: " + withMinSalary());
+        System.out.println("Средняя зарплата сотрудников: " + employeesAverageSalary());
 
     }
-}
+        public static void allEmployees() {
+            for (Employee employee : employees) {
+                if (employee != null) {
+                    System.out.println(employee);
+                }
+            }
+        }
+
+        public static int sumSalary() {
+            int sum = 0;
+            for (Employee employee : employees) {
+                if (employee != null) {
+                    sum += employee.getSalary();
+                }
+            }
+            return sum;
+        }
+
+        public static Employee withMaxSalary(){
+        int max = Integer.MIN_VALUE;
+        Employee maxSalaryEmployee = null;
+        for(Employee employee : employees){
+            if (employee != null && employee.getSalary() > max){
+                max = employee.getSalary();
+                maxSalaryEmployee = employee;
+            }
+        }
+        return maxSalaryEmployee;
+        }
+
+        public static Employee withMinSalary(){
+        int min = Integer.MAX_VALUE;
+        Employee minSalaryEmployee = null;
+        for (Employee employee : employees){
+            if (employee !=null && employee.getSalary() < min){
+                min = employee.getSalary();
+                minSalaryEmployee = employee;
+            }
+        }
+        return minSalaryEmployee;
+        }
+
+        public static double employeesAverageSalary(){
+        int count = employees.length;
+        int sum = sumSalary();
+        double average = (double) sum / count;
+        return average;
+        }
+    }
